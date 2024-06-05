a8aeb1ab38979978067a575bb1c44fcf99e9d8b6
hamza-25 <me677499@gmail.com>
2024-06-06 00:28:25 +0100

    complete addresses methods

--- laravel_11/routes/api.php
+++ laravel_11/routes/api.php
@@ -22,6 +22,7 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::get('/addresses', [AddressController::class, 'index']);
 Route::post('/addresses', [AddressController::class,'store']);
 
+
 Route::get('/orders',[OrderController::class, 'index']);
 Route::post('/orders',[OrderController::class, 'store']);
 
